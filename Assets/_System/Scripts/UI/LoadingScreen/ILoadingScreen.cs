0769f32425a1a5085c3e8dae14aba2f80395cdf3
LukeTrynchuk <luke123t@gmail.com>
2020-01-29 21:32:33 -0800

    Add ILoadingScreen interface

---
+++ Assets/_System/Scripts/UI/LoadingScreen/ILoadingScreen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DogHouse.ToonWorld.UI
+{
+    /// <summary>
+    /// ILoadingScreen is an interface that all
+    /// loading screens must implement. This adds 
+    /// an api for objects to interact with the 
+    /// the different states of a loading screen.
+    /// </summary>
+    public interface ILoadingScreen
+    {
+        Action OnTransitionedIn { get; set; }
+        Action OnTransitionedOut { get; set; }
+
+        void TransitionIn();
+        void TransitionOut();
+
+        void TransitionIn(Action callback);
+        void TransitionOut(Action callback);
+    }
+
+    public enum LoadingScreenState
+    {
+        OFF,
+        TRANSITION_IN,
+        ON,
+        TRANSITION_OUT
+    }
+}
