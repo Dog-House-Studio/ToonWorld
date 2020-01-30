f1af5a437b65d48a05688354f06e2af01f343b23
LukeTrynchuk <luke123t@gmail.com>
2020-01-29 21:49:12 -0800

    Setup basic animation controller

---
+++ Assets/_System/Scripts/UI/LoadingScreen/IconCutoutLoadingScreen.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace DogHouse.ToonWorld.UI
+{
+    /// <summary>
+    /// IconCutoutLoadingScreen is an implementation
+    /// of the ILoadingScreen interface. The Icon Cutout
+    /// effect uses a mask to transition in the loading
+    /// screen.
+    /// </summary>
+    [RequireComponent(typeof(Animator))]
+    public class IconCutoutLoadingScreen : MonoBehaviour, ILoadingScreen
+    {
+        #region Public Variables
+        public Action OnTransitionedIn { get; set; }
+        public Action OnTransitionedOut { get; set; }
+        #endregion
+
+        #region Private Variables
+        private Animator m_animator;
+        private LoadingScreenState m_state = LoadingScreenState.OFF;
+        #endregion
+
+        #region Main Methods
+        private void Awake()
+        {
+            m_animator = GetComponent<Animator>();
+        }
+
+        public void TransitionIn()
+        {
+            
+        }
+
+        public void TransitionIn(Action callback)
+        {
+            
+        }
+
+        public void TransitionOut()
+        {
+            
+        }
+
+        public void TransitionOut(Action callback)
+        {
+            
+        }
+        #endregion
+    }
+}
