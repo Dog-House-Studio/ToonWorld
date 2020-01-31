6f6cd974aa4b189c9d494b2d1f5151dfa2745169
LukeTrynchuk <luke123t@gmail.com>
2020-01-30 18:01:39 -0800

    Setup scene definitions

---
+++ Assets/_System/Scripts/Services/GameSceneManagerService/GameSceneDefinition.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using DogHouse.ScaffoldCandidates.AssetManagement;
+using UnityEngine.SceneManagement;
+
+namespace DogHouse.ToonWorld.Services
+{
+    /// <summary>
+    /// GameSceneDefinition is a scriptable object
+    /// that serializes a scene and how it should be 
+    /// loaded.
+    /// </summary>
+    [CreateAssetMenu(fileName = "MyNewGameSceneDefinition",
+        menuName = "Dog House/Asset Management/Game Scene Definition")]
+    public class GameSceneDefinition : ScriptableObject
+    {
+        #region Private Variables
+        [SerializeField]
+        private AssetToken m_sceneToken;
+
+        [SerializeField]
+        private LoadSceneMode m_mode;
+
+        [SerializeField]
+        private TransitionType m_transitionType;
+        #endregion
+    }
+
+    public enum TransitionType
+    {
+        Fade,
+        LoadingScreen
+    }
+}
