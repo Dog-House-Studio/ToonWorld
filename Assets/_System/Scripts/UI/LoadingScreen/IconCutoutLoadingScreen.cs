4b1d60f6d3bc3b389e1d6f8cc55282631cda8310
LukeTrynchuk <luke123t@gmail.com>
2020-01-30 16:51:27 -0800

    Finish setting up loading screen prefab

--- Assets/_System/Scripts/UI/LoadingScreen/IconCutoutLoadingScreen.cs
+++ Assets/_System/Scripts/UI/LoadingScreen/IconCutoutLoadingScreen.cs
@@ -18,8 +18,19 @@ namespace DogHouse.ToonWorld.UI
         #endregion
 
         #region Private Variables
+        [SerializeField]
+        private GameObject m_maskObject;
+
         private Animator m_animator;
         private LoadingScreenState m_state = LoadingScreenState.OFF;
+        private LoadingScreenState m_targetState;
+
+        private Action m_callbackIn;
+        private Action m_callbackOut;
+
+        private const string ON_PARAM = "On";
+        private const string IDLE_ON = "IdleOn";
+        private const string IDLE_OFF = "IdleOff";
         #endregion
 
         #region Main Methods
@@ -30,22 +41,75 @@ namespace DogHouse.ToonWorld.UI
 
         public void TransitionIn()
         {
-            
+            if (m_state == LoadingScreenState.TRANSITION_IN) return;
+            m_targetState = LoadingScreenState.ON;
+            m_maskObject?.SetActive(true);
         }
 
         public void TransitionIn(Action callback)
         {
-            
+            m_callbackIn += callback;
+            TransitionIn();
         }
 
         public void TransitionOut()
         {
-            
+            if (m_state == LoadingScreenState.TRANSITION_OUT) return;
+            m_targetState = LoadingScreenState.OFF;
+            m_maskObject?.SetActive(true);
         }
 
         public void TransitionOut(Action callback)
         {
-            
+            m_callbackOut += callback;
+            TransitionOut();
+        }
+
+        void Update()
+        {
+            if (m_state == m_targetState) return;
+
+            if(m_state == LoadingScreenState.TRANSITION_IN &&
+                m_animator.GetCurrentAnimatorStateInfo(0).IsName(IDLE_ON))
+            {
+                SetLoadingState(LoadingScreenState.ON);
+                return;
+            }
+
+            if (m_state == LoadingScreenState.TRANSITION_OUT &&
+                m_animator.GetCurrentAnimatorStateInfo(0).IsName(IDLE_OFF))
+            {
+                SetLoadingState(LoadingScreenState.OFF);
+                return;
+            }
+        }
+        #endregion
+
+        #region Utility Methods
+        private void SetAnimatorValue(bool value)
+        {
+            m_animator?.SetBool(ON_PARAM, value);
+        }
+
+        private void SetLoadingState(LoadingScreenState state)
+        {
+            m_state = state;
+            if(m_state == LoadingScreenState.ON)
+            {
+                m_callbackIn?.Invoke();
+                m_callbackIn = null;
+                OnTransitionedIn?.Invoke();
+                return;
+            }
+
+            if (m_state == LoadingScreenState.OFF)
+            {
+                m_callbackOut?.Invoke();
+                m_callbackOut = null;
+                OnTransitionedOut?.Invoke();
+                m_maskObject?.SetActive(false);
+                return;
+            }
         }
         #endregion
     }
