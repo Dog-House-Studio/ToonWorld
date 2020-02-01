a10f06ea78adc41d856d9a6b847a4bbe6aae58d2
LukeTrynchuk <luke123t@gmail.com>
2020-02-01 07:24:06 -0800

    Add map location script

---
+++ Assets/_System/Scripts/Map/MapLocation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace DogHouse.ToonWorld.Map
+{
+    /// <summary>
+    /// MapLocation is a scriptable object that
+    /// represents a particular node in a 
+    /// overworld map. This is used to identify
+    /// the different types of locations that the
+    /// player could go to.
+    /// </summary>
+    [CreateAssetMenu(fileName = "MyNewAssetToken",
+        menuName = "Dog House/ToonWorld/Map Location")]
+    public class MapLocation : ScriptableObject
+    {
+        #region Public Variables
+        public Sprite LocationSprite => m_locationSprite;
+        public string LocationName => m_locationName;
+        #endregion
+
+        #region Private Variables
+        [SerializeField]
+        private Sprite m_locationSprite;
+
+        [SerializeField]
+        private string m_locationName;
+        #endregion
+    }
+}
