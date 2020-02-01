89c7a5bb6fc54a3a6389ae0c24a329b300c1ab8d
LukeTrynchuk <luke123t@gmail.com>
2020-02-01 07:19:29 -0800

    Add map scripts

---
+++ Assets/_System/Scripts/Map/DefaultOverworldMapGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DogHouse.ToonWorld.Map
+{
+    /// <summary>
+    /// DefaultOverworldMapGenerator is a script that
+    /// implements the IOverworldMapGenerator. This 
+    /// implementation creates the map similar to how
+    /// the maps are generated in slay the spire.
+    /// </summary>
+    public class DefaultOverworldMapGenerator : MonoBehaviour, 
+        IOverworldMapGenerator
+    {
+        #region Main Methods
+        public void Display(bool value)
+        {
+            
+        }
+
+        public void Generate()
+        {
+            
+        }
+
+        public void SetSeed(int seedValue)
+        {
+            
+        }
+        #endregion
+    }
+}
