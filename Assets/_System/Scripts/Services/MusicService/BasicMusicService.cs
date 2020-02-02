2b7e154dc794c3d550dbd1eb32b08031175d14d4
LukeTrynchuk <luke123t@gmail.com>
2020-02-02 12:56:07 -0800

    Add music service scripts

---
+++ Assets/_System/Scripts/Services/MusicService/BasicMusicService.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DogScaffold;
+
+namespace DogHouse.ToonWorld.Services
+{
+    /// <summary>
+    /// BasicMusicService is a script that implements
+    /// the IMusicService. The Music Service is 
+    /// responsible for playing music.
+    /// </summary>
+    [RequireComponent(typeof(AudioSource))]
+    public class BasicMusicService : BaseService<IMusicService>,
+        IMusicService
+    {
+        #region Private Variables
+        [SerializeField]
+        private AudioClip[] m_musicTracks;
+
+        private AudioSource m_source;
+        private bool m_isActive = false;
+        #endregion
+
+        #region Main Methods
+        public void Start()
+        {
+            m_source = GetComponent<AudioSource>();
+        }
+
+        public void Play()
+        {
+            m_isActive = true;
+            if (m_source.isPlaying) return;
+            PlayNewAudioTrack();
+        }
+
+        public void Stop()
+        {
+            m_isActive = false;
+            if(m_source.isPlaying)
+            {
+                m_source.Stop();
+            }
+        }
+
+        void Update()
+        {
+            if(m_isActive && !m_source.isPlaying)
+            {
+                PlayNewAudioTrack();
+            }
+        }
+        #endregion
+
+        #region Utility Methods
+        private void PlayNewAudioTrack()
+        {
+            m_source.clip = m_musicTracks[Random.Range(0, m_musicTracks.Length)];
+            m_source.Play();
+        }
+        #endregion
+    }
+}
