7d137c42b8d4e40b5c0cdaa45031a33d8f30d5bd
LukeTrynchuk <luke123t@gmail.com>
2020-02-03 21:15:18 -0800

    Add changing the alpha of settings on pointer enter

---
+++ Assets/_System/Scripts/UI/Color/ChangeImageAlpha.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DogHouse.ToonWorld.UI
+{
+    /// <summary>
+    /// ChangeImageAlpha is a script that changes
+    /// this object's image alpha when told to do
+    /// so.
+    /// </summary>
+    [RequireComponent(typeof(Image))]
+    public class ChangeImageAlpha : MonoBehaviour
+    {
+        #region Private Variables
+        private Image m_image;
+        #endregion
+
+        #region Main Methods
+        private void Start()
+        {
+            m_image = GetComponent<Image>();
+        }
+
+        public void SetAlpha(float value)
+        {
+            m_image.SetAlphaTransparency(value);
+        }
+        #endregion
+    }
+}
