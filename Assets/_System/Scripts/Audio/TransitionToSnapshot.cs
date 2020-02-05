4002721d9b00ef5608ba22b4efc8e9bd269f20b7
LukeTrynchuk <luke123t@gmail.com>
2020-02-04 19:13:43 -0800

    Add snapshot transitions to settings menu

---
+++ Assets/_System/Scripts/Audio/TransitionToSnapshot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace DogHouse.ToonWorld.Audio
+{
+    /// <summary>
+    /// TransitionToSnapshot is a script that transitions
+    /// the current audio snapshot to a given one.
+    /// </summary>
+    public class TransitionToSnapshot : MonoBehaviour
+    {
+        #region Private Variables
+        [SerializeField]
+        private AudioMixerSnapshot m_snapshot;
+
+        [SerializeField]
+        private float m_transitionTime;
+        #endregion
+
+        #region Main Methods
+        public void Transition()
+        {
+            m_snapshot?.TransitionTo(m_transitionTime);
+        }
+        #endregion
+    }
+}
