35650300d6dd51ff71232ef35a3b57b722bcb518
LukeTrynchuk <luke123t@gmail.com>
2020-02-05 16:59:47 -0800

    Resolve selected icon on map bug

--- Assets/_System/Scripts/Services/MapService/MapService.cs
+++ Assets/_System/Scripts/Services/MapService/MapService.cs
@@ -41,6 +41,8 @@ namespace DogHouse.ToonWorld.Services
 
         private NodeWeb m_nodeWeb;
         private Node m_currentNode = null;
+        private List<Node> m_availableOptionNodes = new List<Node>();
+
         private Node m_zoomedNode = null;
 
         private ServiceReference<IGameSceneManagerService> m_sceneManager 
@@ -57,12 +59,9 @@ namespace DogHouse.ToonWorld.Services
             generator = FindObjectsOfType<MonoBehaviour>().OfType<IOverworldMapGenerator>().FirstOrDefault();
             m_nodeWeb = generator.Generate();
 
-            List<Node> startingNodes = generator.FetchStartingAvailableNodes();
-            
-            for(int i = 0; i < startingNodes.Count; i++)
-            {
-                startingNodes[i].SetAsActiveOption(true);
-            }
+
+            m_availableOptionNodes = generator.FetchStartingAvailableNodes();
+            SetAvailableNodes(true);
         }
 
         public override void OnEnable()
@@ -82,7 +81,9 @@ namespace DogHouse.ToonWorld.Services
         public void ReportIconSelected(Node icon)
         {
             m_zoomedNode = icon;
-            m_currentNode?.SetAsCurrent(false);
+            //m_currentNode?.SetAsCurrent(false);
+            SetAvailableNodes(false);
+
             icon.SetIconSelectedColor(true);
 
             m_iconCamera.transform.position 
@@ -107,7 +108,8 @@ namespace DogHouse.ToonWorld.Services
             m_mapCamera.gameObject.SetActive(true);
 
             m_UIObject?.SetActive(false);
-            m_currentNode?.SetAsCurrent(true);
+            //m_currentNode?.SetAsCurrent(true);
+            SetAvailableNodes(true);
         }
 
         private void GoToNodeScene()
@@ -136,6 +138,17 @@ namespace DogHouse.ToonWorld.Services
             SceneManager.UnloadScene(sceneName);
             m_loadingScreenService.Reference.TransitionOut();
             ReturnToMapView();
+        }
+        #endregion
+
+        #region Utility Methods
+        private void SetAvailableNodes(bool value)
+        {
+            foreach (Node node in m_nodeWeb.Nodes)
+                node?.SetAsActiveOption(false);
+
+            foreach (Node node in m_availableOptionNodes)
+                node?.SetAsActiveOption(value);
         }
         #endregion
     }
