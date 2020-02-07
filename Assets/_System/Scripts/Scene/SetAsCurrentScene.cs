6cc87a391ea13eb4de64bf3b006a5ff50e1f0d66
LukeTrynchuk <luke123t@gmail.com>
2020-02-06 18:29:09 -0800

    Resolve blown out lighting issue

---
+++ Assets/_System/Scripts/Scene/SetAsCurrentScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DogHouse.ToonWorld.Scene
+{
+    /// <summary>
+    /// SetAsCurrentScene will set the current
+    /// object's scene as the current active scene.
+    /// </summary>
+    public class SetAsCurrentScene : MonoBehaviour
+    {
+        #region Main Methods
+        private void LateUpdate()
+        {
+            SceneManager.SetActiveScene(gameObject.scene);
+            this.enabled = false;
+        }
+        #endregion
+    }
+}
