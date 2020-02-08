0dd6790d00370f9c99fe2e5c51e54f3548969051
LukeTrynchuk <luke123t@gmail.com>
2020-02-08 14:23:47 -0800

    Add text lerping

--- Assets/_System/Scripts/UI/Battle/ExperienceBarController.cs
+++ Assets/_System/Scripts/UI/Battle/ExperienceBarController.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
+using System;
 
 namespace DogHouse.ToonWorld.CombatControllers
 {
@@ -11,11 +11,56 @@ namespace DogHouse.ToonWorld.CombatControllers
     public class ExperienceBarController : MonoBehaviour
     {
         #region Private Variables
+        [Header("Elements")]
+        [SerializeField]
+        private TMP_Text m_percentageText;
 
+        [SerializeField]
+        private TMP_Text m_levelUpText;
+
+        [Header("Lerping")]
+        [SerializeField]
+        private AnimationCurve m_lerpCurve;
+
+        [SerializeField]
+        [Range(0.0001f, 10f)]
+        private float m_lerpScalar;
+
+        [SerializeField]
+        [Range(0.0001f, 10f)]
+        private float m_lerpTime;
+        private float m_currentTime;
+
+        [Header("Value")]
+        [Range(0f, 1f)]
+        [SerializeField]
+        private float m_progressAmount;
+        private float m_slowAmount = 0f;
         #endregion
 
         #region Main Methods
-        
+        private void Update()
+        {
+            if(Mathf.Approximately(m_slowAmount, m_progressAmount))
+            {
+                m_currentTime = 0f;
+                return;
+            }
+
+            m_currentTime += Time.deltaTime;
+            float lerp = Mathf.Clamp01(m_currentTime / m_lerpTime);
+            lerp = m_lerpCurve.Evaluate(lerp);
+
+            m_slowAmount = Mathf.Lerp(m_slowAmount, m_progressAmount, lerp * m_lerpScalar * Time.deltaTime);
+            SetText(m_slowAmount);
+        }
+
+        private void SetText(float value)
+        {
+            value = value * 100f;
+            string text = value.ToString("0");
+            m_percentageText.text = text + "%";
+        }
         #endregion
     }
 }
