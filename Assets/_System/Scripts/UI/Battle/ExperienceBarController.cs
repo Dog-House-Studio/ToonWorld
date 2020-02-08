4a7ec2fa297299aff262532c547733fe13eae93c
LukeTrynchuk <luke123t@gmail.com>
2020-02-08 13:50:01 -0800

    Add controller for experience ui

---
+++ Assets/_System/Scripts/UI/Battle/ExperienceBarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DogHouse.ToonWorld.CombatControllers
+{
+    /// <summary>
+    /// ExperienceBarController is a script that controls the
+    /// experience bar visuals.
+    /// </summary>
+    public class ExperienceBarController : MonoBehaviour
+    {
+        #region Private Variables
+
+        #endregion
+
+        #region Main Methods
+        
+        #endregion
+    }
+}
