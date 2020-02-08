40f69cd29a8f1e52a6e44050e40e3a28d89ecee1
LukeTrynchuk <luke123t@gmail.com>
2020-02-08 08:40:14 -0800

    Setup pedestal spawner

---
+++ Assets/_System/Scripts/Unit/UnitSelectionCreator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace DogHouse.ToonWorld.Unit
+{
+    /// <summary>
+    /// UnitSelectionController is a script that controls
+    /// the creation of the player selection.
+    /// </summary>
+    public class UnitSelectionCreator : MonoBehaviour
+    {
+        #region Private Variables
+        [SerializeField]
+        private GameObject m_pedestalPrefab;
+
+        [SerializeField]
+        private int m_numberOfOptions;
+
+        [SerializeField]
+        private float m_spacing;
+        #endregion
+
+        #region Main Methods
+        private void Start()
+        {
+            float offset = m_numberOfOptions * m_spacing;
+            Vector3 position = this.transform.position;
+            position.x -= offset * 0.5f;
+
+            for(int i = 0; i < m_numberOfOptions; i++)
+            {
+                CreatePedestal(position);
+                position.x += m_spacing;
+            }
+        }
+        #endregion
+
+        #region Utility Methods
+        private void CreatePedestal(Vector3 pos)
+        {
+            GameObject pedestal = Instantiate(m_pedestalPrefab);
+            pedestal.transform.position = pos;
+            pedestal.transform.SetParent(transform);
+        }
+        #endregion
+    }
+}
