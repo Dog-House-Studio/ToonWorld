2babf604a50cd075224598d3b756eb390a866d06
LukeTrynchuk <luke123t@gmail.com>
2020-02-08 23:18:27 -0800

    Add different animation for overlay

---
+++ Assets/_System/Scripts/UI/Battle/ExperienceBarOverlayController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DogHouse.ToonWorld.UI
+{
+    /// <summary>
+    /// ExperienceBarOverlayController is a script that controls
+    /// the overlay on the experience bar.
+    /// </summary>
+    public class ExperienceBarOverlayController : MonoBehaviour
+    {
+        #region Private Variables
+        [SerializeField]
+        private Animator m_animator;
+        #endregion
+
+        #region Main Methods
+        public void SetEnabled(bool value)
+        {
+
+        }
+        #endregion
+    }
+}
