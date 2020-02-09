2beaf4aeae39aa0faf116dd065990d1f3109521a
LukeTrynchuk <luke123t@gmail.com>
2020-02-08 21:50:06 -0800

    Fade in color on bar

--- Assets/_System/Scripts/UI/Battle/ExperienceBarSlotController.cs
+++ Assets/_System/Scripts/UI/Battle/ExperienceBarSlotController.cs
@@ -12,6 +12,22 @@ namespace DogHouse.ToonWorld.UI
         #region Private Variables
         [SerializeField]
         private Image m_fillImage;
+
+        [SerializeField]
+        [Range(0.0001f, 1f)]
+        private float m_transitionTime;
+
+        [SerializeField]
+        private AnimationCurve m_lerpCurve;
+
+        [SerializeField]
+        private float m_scaleAmount;
+
+        private bool m_animating = false;
+        private float m_lerp = 0f;
+        private float m_timePassed = 0f;
+        private bool m_filling = false;
+        private Color m_color;
         #endregion
 
         #region Main Methods
@@ -19,21 +35,30 @@ namespace DogHouse.ToonWorld.UI
         {
             col.a = m_fillImage.color.a;
             m_fillImage.color = col;
+            m_color = col;
         }
 
         public void SetFilled(bool value)
         {
-            Color c = m_fillImage.color;
+            m_animating = true;
+            m_filling = value;
+            m_timePassed = 0f;
+            m_lerp = 0f;
+        }
 
-            if(value)
+        private void Update()
+        {
+            if (!m_animating) return;
+            if(Mathf.Approximately(m_lerp, 1f))
             {
-                c.a = 1f;
-                m_fillImage.color = c;
-                return;
+                m_animating = false;
             }
 
-            c.a = 0f;
-            m_fillImage.color = c;
+            m_timePassed += Time.deltaTime;
+            m_lerp = m_lerpCurve.Evaluate(Mathf.Clamp01(m_timePassed / m_transitionTime));
+
+            m_color.a = (m_filling) ? m_lerp : 1 - m_lerp;
+            m_fillImage.color = m_color;
         }
         #endregion
     }
