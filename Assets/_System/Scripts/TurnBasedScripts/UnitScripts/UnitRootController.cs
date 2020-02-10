f8d48ca6706b22019fe0890743d9a08351a0a6cb
LukeTrynchuk <luke123t@gmail.com>
2020-02-09 16:37:35 -0800

    Add unit root controller script

---
+++ Assets/_System/Scripts/TurnBasedScripts/UnitScripts/UnitRootController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace DogHouse.ToonWorld.CombatControllers
+{
+    /// <summary>
+    /// UnitRootController is a script that controls
+    /// the root of a unit on the battlefield. 
+    /// 
+    /// This should be able to setup and create all 
+    /// visuals for the root object given a unit 
+    /// definition file.
+    /// </summary>
+    public class UnitRootController : MonoBehaviour
+    {
+        #region Private Variables
+        
+        #endregion
+
+        #region Main Methods
+
+        #endregion
+    }
+}
