db9c96f3c85f40a8317faad8cf4dbc26550d6bd2
LukeTrynchuk <luke123t@gmail.com>
2020-02-09 17:23:06 -0800

    Add IUnitIdentifier interface

---
+++ Assets/_System/Scripts/TurnBasedScripts/UnitScripts/IUnitIdentifier.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DogHouse.ToonWorld.CombatControllers
+{
+    /// <summary>
+    /// IUnitIdentifier is an interface that when
+    /// implemented, is implemented by something that
+    /// identifies a unit's information. When this happens,
+    /// the concrete implementation must set the given
+    /// information about a particular unit.
+    /// </summary>
+    public interface IUnitIdentifier
+    {
+        void SetName(string name);
+        void SetClassName(string className);
+        void SetClassEmblem(Sprite sprite);
+        void SetUnitLevel(int level);
+    }
+}
