fb409fff105bc4c48e87409fe866a88db255d4ca
LukeTrynchuk <luke123t@gmail.com>
2020-02-14 19:03:01 -0800

    Add lerpable class

---
+++ Assets/_System/Scripts/Animation/Lerpable.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+
+namespace DogHouse.ToonWorld.Animation
+{
+    /// <summary>
+    /// Lerpable is a script that lerps some
+    /// value. The value that is lerped can be
+    /// used for animation purposes.
+    /// </summary>
+    [Serializable]
+    public class Lerpable
+    {
+        #region Public Variables
+        public float LerpValue => m_lerpValue;
+        #endregion
+
+        #region Private Variables
+        [SerializeField]
+        private AnimationCurve m_lerpCurve;
+
+        [SerializeField]
+        [Range(0f,1f)]
+        private float m_lerpTime;
+
+        private float m_timePassed;
+        private float m_lerpValue;
+        private bool m_isAnimating;
+        #endregion
+
+        #region Main Methods
+        public void BeginLerping()
+        {
+            m_isAnimating = true;
+            m_timePassed = 0f;
+        }
+
+        void Update()
+        {
+            if (!m_isAnimating) return;
+
+            m_timePassed += Time.deltaTime;
+            m_timePassed = Mathf.Clamp(m_timePassed, 0f, m_lerpTime);
+            m_lerpValue = m_lerpCurve.Evaluate(m_timePassed / m_lerpTime);
+
+            if(Mathf.Approximately(m_lerpTime, m_timePassed))
+            {
+                m_isAnimating = false;
+            }
+        }
+        #endregion
+    }
+}
