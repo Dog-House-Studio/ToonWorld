a67702daf831a8fa98e7813f788431b0e26c5587
LukeTrynchuk <luke123t@gmail.com>
2020-02-16 11:22:39 -0800

    Validate based on shared tiles

--- Assets/_System/Scripts/Services/MeshZones/MeshZoneService.cs
+++ Assets/_System/Scripts/Services/MeshZones/MeshZoneService.cs
@@ -272,7 +272,6 @@ namespace DogHouse.ToonWorld.Services
         private List<Connection> GenerateConnections(List<Vector3> edgeVertices, List<Vector3> edgeTiles)
         {
             List<Connection> connections = new List<Connection>();
-            List<int> badConnections = new List<int>();
 
             for(int i = 0; i < edgeVertices.Count; i++)
             {
@@ -304,15 +303,14 @@ namespace DogHouse.ToonWorld.Services
                         node.connections.Add(connections[j]);
                     }
                 }
-
-                if(node.connections.Count > 2)
-                {
-                    badConnections.Add(i);
-                }
-
                 connections[i] = node;
             }
-            
+
+            //Remove invalid connections
+            for(int i = 0; i < connections.Count; i++)
+            {
+                connections[i].RemoveInvalidConnections();
+            }
 
             return connections;
         }
@@ -324,5 +322,37 @@ namespace DogHouse.ToonWorld.Services
         public Vector3 root;
         public List<Connection> connections = new List<Connection>();
         public List<Vector3> sharedTiles = new List<Vector3>();
+
+        public void RemoveInvalidConnections()
+        {
+            for(int i = connections.Count - 1; i >= 0; i--)
+            {
+                if(!isValidConnection(this,connections[i]))
+                {
+                    connections.RemoveAt(i);
+                }
+            }
+        }
+
+        public static bool isValidConnection(Connection a, Connection b)
+        {
+            //This shouldn't be hardcoded
+            if (!Approximately(Vector3.Distance(a.root, b.root), 1f)) return false;
+
+            int sharedCount = 0;
+            for(int i = 0; i < a.sharedTiles.Count; i++)
+            {
+                for(int j = 0; j < b.sharedTiles.Count; j++)
+                {
+                    if(a.sharedTiles[i].Equals(b.sharedTiles[j]))
+                    {
+                        sharedCount++;
+                        if (sharedCount > 1) return false;
+                    }
+                }
+            }
+
+            return (sharedCount == 1);
+        }
     }
 }
