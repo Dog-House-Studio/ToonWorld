6e2c185b28c12e5a7e3bbe3dde812e52013784f5
LukeTrynchuk <luke123t@gmail.com>
2020-02-16 16:24:19 -0800

    Add merging logic

--- Assets/_System/Scripts/Services/MeshZones/Connections/ConnectionRing.cs
+++ Assets/_System/Scripts/Services/MeshZones/Connections/ConnectionRing.cs
@@ -55,9 +55,20 @@ namespace DogHouse.ToonWorld.Services
             return false;
         }
 
+        public static bool IsMergable(ConnectionRing a, ConnectionRing b)
+        {
+            if (a.Ring.Count == 0) return false;
+            if (b.Ring.Count == 0) return false;
+            if (a.Ring[0] == b.Ring[0]) return true;
+            if (a.Ring[0] == b.Ring[b.Ring.Count - 1]) return true;
+            if (a.Ring[a.Ring.Count - 1] == b.Ring[0]) return true;
+            if (a.Ring[a.Ring.Count - 1] == b.Ring[b.Ring.Count - 1]) return true;
+            return false;
+        }
+
         public void JoinRing(ConnectionRing ring)
         {
-            int connectionPoint = CalculateConnectionPoint(ring);
+            int connectionPoint = CalculateJoinPoint(ring);
             if(connectionPoint == -1)
             {
                 UnityEngine.Debug.LogError("Connection point error");
@@ -66,8 +77,8 @@ namespace DogHouse.ToonWorld.Services
 
             if (connectionPoint == 0) Ring.Reverse();
 
-            int joiningRingConnectionPoint = ring.CalculateConnectionPoint(this);
-            if(connectionPoint == -1)
+            int joiningRingConnectionPoint = ring.CalculateJoinPoint(this);
+            if(joiningRingConnectionPoint == -1)
             {
                 UnityEngine.Debug.LogError("Connection point error");
                 return;
@@ -77,7 +88,29 @@ namespace DogHouse.ToonWorld.Services
             Ring.AddRange(ring.Ring);
         }
 
-        public int CalculateConnectionPoint(ConnectionRing joining)
+        public void MergeRing(ConnectionRing ring)
+        {
+            int mergePoint = CalculateMergePoint(ring);
+            if (mergePoint == -1)
+            {
+                UnityEngine.Debug.LogError("Merge point error");
+                return;
+            }
+
+            if (mergePoint == 0) Ring.Reverse();
+
+            int mergingRingConnectionPoint = ring.CalculateMergePoint(this);
+            if(mergingRingConnectionPoint == -1)
+            {
+                UnityEngine.Debug.LogError("Merge point error");
+                return;
+            }
+
+            if (mergingRingConnectionPoint != 0) ring.Ring.Reverse();
+            Ring.AddRange(ring.Ring);
+        }
+
+        public int CalculateJoinPoint(ConnectionRing joining)
         {
             if (Ring.Count == 0) return -1;
             if (joining.Ring.Count == 0) return -1;
@@ -85,6 +118,17 @@ namespace DogHouse.ToonWorld.Services
             if (Ring[0].HasConnection(joining.Ring[joining.Ring.Count - 1])) return 0;
             if (Ring[Ring.Count - 1].HasConnection(joining.Ring[0])) return Ring.Count - 1;
             if (Ring[Ring.Count - 1].HasConnection(joining.Ring[joining.Ring.Count - 1])) return Ring.Count - 1;
+            return -1;
+        }
+
+        public int CalculateMergePoint(ConnectionRing merging)
+        {
+            if (Ring.Count == 0) return -1;
+            if (merging.Ring.Count == 0) return -1;
+            if (Ring[0] == merging.Ring[0]) return 0;
+            if (Ring[0] == merging.Ring[merging.Ring.Count - 1]) return 0;
+            if (Ring[Ring.Count - 1] == merging.Ring[0]) return Ring.Count - 1;
+            if (Ring[Ring.Count - 1] == merging.Ring[merging.Ring.Count - 1]) return Ring.Count - 1;
             return -1;
         }
     }
