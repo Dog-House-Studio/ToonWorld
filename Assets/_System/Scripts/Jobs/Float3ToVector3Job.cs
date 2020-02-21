d68b2a34d88a2b0802ef002867d7e690d7e3857d
LukeTrynchuk <luke123t@gmail.com>
2020-02-21 10:45:24 -0800

    Write float3 to vector3 job

---
+++ Assets/_System/Scripts/Jobs/Float3ToVector3Job.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Unity.Burst;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Mathematics;
+
+namespace DogHouse.ToonWorld.Jobs
+{
+    /// <summary>
+    /// Float3ToVector3Job is a job that converts an
+    /// array of float3 to an array of vector3.
+    /// </summary>
+    [BurstCompile(CompileSynchronously = true)]
+    public class Float3ToVector3Job : IJobParallelFor
+    {
+        [ReadOnly]
+        public NativeArray<float3> input;
+
+        [WriteOnly]
+        public NativeArray<Vector3> output;
+
+        public void Execute(int index)
+        {
+            output[index] = new Vector3(input[index].x, input[index].y, input[index].z);
+        }
+    }
+}
--- Assets/_System/Scripts/Jobs/Vector3ToFloat3Job.cs
+++ Assets/_System/Scripts/Jobs/Vector3ToFloat3Job.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Unity.Burst;
 using Unity.Jobs;
 using Unity.Mathematics;
